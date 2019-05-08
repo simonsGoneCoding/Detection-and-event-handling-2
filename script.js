875cee34ca0e5bc982e6d43664437d3d7f54f4cc
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-05-08 21:21:14 +0100

    add commets

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const div = document.querySelector('div');
-let divX = 150; //pozycja diva
+let divX = 150; //div position 
 let divY = 50;
 div.style.left = `${divX}px`;
 div.style.top = `${divY}px`;
 
-let drawActive = false // flaga (czyli dajemy to if-a, jak jest prawda to sie wykonuje)
+let drawActive = false // flag
 
-let insideDivX; // zmienne okreslajace polozenie kursora w divie. 
-let insideDivY; // zainicjalizowane w zasiegu globalnym zeby mozna bylo pobierac w roznych funkcjach
+let insideDivX; // variables determing cursor position inside div. 
+let insideDivY;
 
-div.addEventListener('mousedown', (e) => { // e = event
-    drawActive = !drawActive; // drawActive = true (!false)
+div.addEventListener('mousedown', (e) => {
+    drawActive = !drawActive;
     div.style.backgroundColor = '#ccc';
-    insideDivX = e.offsetX; //event.offsetX <- podaje pozycje na osi X w srodku diva 
+    insideDivX = e.offsetX; //event.offsetX <- gives position on X axis inside div
     insideDivY = e.offsetY;
     console.log(insideDivX, insideDivY);
 })
 
 div.addEventListener('mousemove', (e) => {
     if (drawActive) {
-        divX = e.clientX - insideDivX; // clientX podaje pozycje wzgledem okna przegladarki, od lewej minus pozycja w srodku diva (dziala jak calc)
+        divX = e.clientX - insideDivX;
         divY = e.clientY - insideDivY;
         div.style.left = `${divX}px`;
         div.style.top = `${divY}px`;
@@ -27,10 +27,7 @@ div.addEventListener('mousemove', (e) => {
 })
 
 div.addEventListener('mouseup', () => {
-    // console.log('puszczamy')
+    // console.log('drop')
     drawActive = !drawActive;
     div.style.backgroundColor = '#000';
 })
-
-
-// DONE
